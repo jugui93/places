4a0f4de8130168ba37cd17d7e0794b002ddf378b
Juan Morales <juanguillermo93@gmail.com>
2023-05-21 19:34:05 -0500

    mejora en la captura del usuario

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -73,7 +73,7 @@ const createPlace = async (req, res, next) => {
         console.log(errors); 
         next(new HttpError('Invalid inputs passed, please check your data.', 422 ));
     }
-    const { title, description, address, creator } = req.body;
+    const { title, description, address } = req.body;
 
     let coordinates;
     
@@ -90,12 +90,12 @@ const createPlace = async (req, res, next) => {
         location: coordinates,
         address,
         image:req.file.path,
-        creator
+        creator: req.userData.userId
     });
 
     let user;
     try {
-        user = await User.findById(creator);
+        user = await User.findById(req.userData.userId);
     } catch (err) {
         const error = new HttpError(
           "Creating place failed, please try again",
